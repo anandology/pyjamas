f3399ed8cfad3f09c4a11eaac4db9c9f5fbfedc2
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-01-01 19:32:58 +0000

    add a grid edit example
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@171 7a2bd370-bda8-463c-979e-2900ccfb811e

---
+++ examples/gridedit/GridEdit.py
@@ -0,0 +1,70 @@
+from pyjamas.ui import Button, RootPanel
+from pyjamas.ui import Label, Grid, CellFormatter, RowFormatter
+from pyjamas.ui import HTMLTable, TextBox
+from pyjamas.ui import KeyboardListener
+from pyjamas import Window
+
+
+class GridEdit:
+    def onModuleLoad(self):
+        
+        self.input = TextBox()
+        self.input.setEnabled(False)
+        self.input.addKeyboardListener(self)
+
+        self.g=Grid()
+        self.g.resize(5, 5)
+        self.g.setHTML(0, 0, "<b>Grid Edit</b>")
+        self.g.setBorderWidth(2)
+        self.g.setCellPadding(4)
+        self.g.setCellSpacing(1)
+        self.g.setWidth("500px")
+        self.g.setHeight("120px")
+        self.g.addTableListener(self)
+        
+        self.initGrid()
+        RootPanel().add(self.input)
+        RootPanel().add(self.g)
+
+    def onKeyDown(self, sender, keycode, modifiers):
+        pass
+
+    def onKeyUp(self, sender, keycode, modifiers):
+        pass
+
+    def onKeyPress(self, sender, keycode, modifiers):
+        if keycode == KeyboardListener.KEY_ESCAPE:
+            self.input.setEnabled(False)
+        elif keycode == KeyboardListener.KEY_ENTER:
+            self.input.setEnabled(False)
+            val = self.input.getText()
+            self.set_grid_value(self.row, self.col, val)
+            
+    def onCellClicked(self, sender, row, col):
+        self.row = row
+        self.col = col
+        val = self.values[row][col]
+        self.input.setText(val)
+        self.input.setEnabled(True)
+
+    def set_grid_value(self, row, col, val):
+        self.values[y][x] = val
+        if val == "":
+            val = "&nbsp;"
+        self.g.setHTML(row, col, val)
+
+    def initGrid(self):
+        
+        self.values = {}
+        for y in range(5):
+            self.values[y] = {}
+            for x in range(5):
+                self.values[y][x] = ""
+        for y in range(5):
+            for x in range(5):
+                val = self.values[y][x]
+                self.set_grid_value(y, x, val)
+
+
+
+
