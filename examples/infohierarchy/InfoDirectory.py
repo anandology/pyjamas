6f1e57e6d3705c38a7ce9914e8dbd74339b7672e
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-01-07 13:57:33 +0000

    right panel grid
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@184 7a2bd370-bda8-463c-979e-2900ccfb811e

--- examples/infohierarchy/InfoDirectory.py
+++ examples/infohierarchy/InfoDirectory.py
@@ -1,22 +1,74 @@
 from pyjamas.ui import RootPanel, HTML, Label, HasAlignment, Button
 from pyjamas.ui import HorizontalPanel, AbsolutePanel, ScrollPanel, Grid
+from pyjamas.ui import DockPanel, HasHorizontalAlignment
 from pyjamas import Window
 
 from pyjamas.horizsplitpanel import HorizontalSplitPanel
 
 from Trees import Trees
 
-class MidPanel(Grid):
+class RightGrid(DockPanel):
+
+    def __init__(self, name):
+        DockPanel.__init__(self)
+        self.grid = Grid()
+        self.grid.resize(5, 8)
+        title = HTML(name)
+        self.add(title, DockPanel.NORTH)
+        self.setCellHorizontalAlignment(title,
+                                        HasHorizontalAlignment.ALIGN_CENTER)
+        self.add(self.grid, DockPanel.CENTER)
+        self.grid.setBorderWidth("1px")
+        self.grid.setCellSpacing("5px")
+
+        for row in range(5):
+            for col in range(8):
+                self.grid.setHTML(row, col, str(row*col))
+
+class RightPanel(Grid):
 
     def __init__(self):
         Grid.__init__(self)
         self.resize(1, 1)
 
+    def set_items(self, item_names):
+
+        self.resizeRows(len(item_names))
+        for i in range(len(item_names)):
+            self.setWidget(i, 0, RightGrid(item_names[i]))
+
+class MidPanel(Grid):
+
+    def __init__(self, sink):
+        Grid.__init__(self)
+        self.resize(1, 1)
+        self.addTableListener(self)
+        self.sink = sink
+        self.selected_row = -1
+
     def set_items(self, items):
 
+        if self.selected_row != -1:
+            self.styleRow(self.selected_row, False)
+
+        self.item_names = []
         self.resizeRows(len(items))
         for i in range(len(items)):
             self.setHTML(i, 0, items[i])
+            self.item_names.append(items[i])
+
+    def onCellClicked(self, sender, row, col):
+        self.styleRow(self.selected_row, False)
+        self.selected_row = row
+        self.styleRow(self.selected_row, True)
+        self.sink.select_right_grid(self.item_names[row])
+        
+    def styleRow(self, row, selected):
+        if (row != -1):
+            if (selected):
+                self.getRowFormatter().addStyleName(row, "midpanel-SelectedRow")
+            else:
+                self.getRowFormatter().removeStyleName(row, "midpanel-SelectedRow")
 
 class InfoDirectory:
 
@@ -46,17 +98,16 @@ class InfoDirectory:
         self.hp.add(self.horzpanel2)
         self.hp.setBorderWidth("1px")
 
-        randomText = ""
-        for i in range(200):
-            randomText += "hello %d " % i
+        self.rp = RightPanel()
 
-        self.horzpanel2.setLeftWidget(HTML(randomText))
-        self.horzpanel2.setRightWidget(HTML(randomText))
+        self.horzpanel2.setLeftWidget(HTML(""))
+        #self.horzpanel2.setRightWidget(HTML(randomText))
+        self.horzpanel2.setRightWidget(self.rp)
 
         self.horzpanel1.add(self.sp)
         self.horzpanel1.add(self.hp)
 
-        self.midpanel = MidPanel()
+        self.midpanel = MidPanel(self)
 
         RootPanel().add(self.horzpanel1)
 
@@ -66,6 +117,7 @@ class InfoDirectory:
         self.onWindowResized(width, height)
         Window.addWindowResizeListener(self)
   
+
     def onWindowResized(self, width, height):
         self.hp.setWidth("%dpx" % (width - self.tree_width))
         self.hp.setHeight("%dpx" % (height - 20))
@@ -83,6 +135,7 @@ class InfoDirectory:
             return
 
         self.set_mid_panel(obj.text)
+        self.clear_right_panel()
 
     def clear_right_panel(self):
         self.horzpanel2.setRightWidget(HTML(""))
@@ -101,4 +154,15 @@ class InfoDirectory:
         self.midpanel.set_items(fake_items)
 
         self.horzpanel2.setLeftWidget(self.midpanel)
+
+    def select_right_grid(self, fake_text):
+        
+        self.horzpanel2.setRightWidget(self.rp)
+
+        fake_items = []
+        for i in range(len(fake_text)):
+            fake_item = "%d - " % (i+1) + fake_text
+            fake_items.append(fake_item)
+
+        self.rp.set_items(fake_items)
 
