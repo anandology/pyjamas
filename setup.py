926a3a0232406fc7c8faa29be3833efd4d5b544a
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-03-14 14:13:01 +0000

    bit of a mess on the setup.py for library install.
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@390 7a2bd370-bda8-463c-979e-2900ccfb811e

--- setup.py
+++ setup.py
@@ -28,27 +28,17 @@ else:
     # it musn't be a fixed path, it must be a relative path.
     datadir = "/usr/share/pyjamas"
 
-lib_data_files = glob.glob("library/*.py")
-lib_ui_data_files = glob.glob("library/ui/*.py")
 bp_data_files = glob.glob("builder/boilerplate/*")
 test_files = glob.glob("pyjs/tests/*")
 stub_files = glob.glob("stubs/*")
-lib_data_files += glob.glob("library/*.js")
-builtin_data_files = glob.glob("library/builtins/*.py")
-platform_data_files = glob.glob("library/platform/*.py")
-pyjamas_data_files = glob.glob("library/pyjamas/*.py")
 addons_data_files = glob.glob("addons/*.py")
 
-data_files = [(os.path.join(datadir, "library"), lib_data_files),
-              (os.path.join(datadir, "library/builtins"), builtin_data_files),
-              (os.path.join(datadir, "library/ui"), lib_ui_data_files),
-              (os.path.join(datadir, "builder/boilerplate"), bp_data_files),
-              (os.path.join(datadir, "pyjs/tests"), test_files),
-              (os.path.join(datadir, "stubs"), stub_files),
-              (os.path.join(datadir, "library/platform"), platform_data_files),
-              (os.path.join(datadir, "library/pyjamas"), pyjamas_data_files),
-              (os.path.join(datadir, "addons"), addons_data_files)
-              ]
+data_files = [
+    (os.path.join(datadir, "builder/boilerplate"), bp_data_files),
+    (os.path.join(datadir, "pyjs/tests"), test_files),
+    (os.path.join(datadir, "stubs"), stub_files),
+    (os.path.join(datadir, "addons"), addons_data_files)
+]
 
 # main purpose of this function is to exclude "output" which
 # could have been built by a developer.
@@ -63,21 +53,27 @@ def get_files(d):
         if fname[-4:] == ".pyc": # ehmm.. noooo.
             continue 
         if os.path.isdir(p):
-            res += get_files(p)
+            get_dir(p)
         else:
             res.append(p)
     return res
 
-# ok - examples is a bit of a pain.  
-for d in glob.glob("examples/*"):
-    if os.path.isdir(d):
-        (pth, fname) = os.path.split(d)
-        expath = get_files(d)
-        pth = os.path.join(os.path.join(datadir, "examples"), fname)
-        #print pth, expath
-        data_files.append((pth, expath))
-    else:
-        data_files.append((os.path.join(datadir, "examples"), [d]))
+def get_dir(dirname):
+    for d in glob.glob("%s/*" % dirname):
+        if os.path.isdir(d):
+            (pth, fname) = os.path.split(d)
+            expath = get_files(d)
+            pth = os.path.join(os.path.join(datadir, dirname), fname)
+            data_files.append((pth, expath))
+        else:
+            data_files.append((os.path.join(datadir, dirname), [d]))
+
+# recursively grab the library and the examples subdirectories - all contents
+get_dir("library")
+get_dir("examples")
+
+from pprint import pprint
+pprint(data_files)
 
 if __name__ == '__main__':
     setup(name = "Pyjamas",
