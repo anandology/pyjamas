37c4846a655e8b53bc50cc4514e443d135b99155
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-04-10 17:07:55 +0000

    more java2py
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@528 7a2bd370-bda8-463c-979e-2900ccfb811e

--- contrib/java2py.py
+++ contrib/java2py.py
@@ -5,13 +5,43 @@
 
 import sys
 
+def countspaces(txt):
+    count = 0
+    while count < len(txt) and txt[count] == ' ':
+        count += 1
+    return count
 def java2pythonlinebyline(txt):
     if txt.find('if (') >= 0:
         txt = txt.replace('if (', 'if ')
         txt = txt.replace(') {', ':')
+    elif txt.find('class ') >= 0 and txt.endswith("{"):
+        txt = txt.replace('{', ':')
+    elif txt.find('while (') >= 0:
+        txt = txt.replace('while (', 'while ')
+        txt = txt.replace(') {', ':')
+    elif txt.find('for (') >= 0:
+        txt = txt.replace('for (', 'for ')
+        txt = txt.replace(') {', ':')
+    count = countspaces(txt)
+    if txt[count:].startswith("}"):
+        txt = count * ' ' + txt[count+1:]
+
+    if txt[count:].startswith("protected ") >= 0:
+        txt = txt.replace("protected ", "")
+    if txt[count:].startswith("public ") >= 0:
+        txt = txt.replace("public ", "")
+    if txt[count:].startswith("private ") >= 0:
+        txt = txt.replace("private ", "")
+    if txt[count:].startswith("static ") >= 0:
+        txt = txt.replace("static ", "")
+    if txt[count:].startswith("final ") >= 0:
+        txt = txt.replace("final ", "")
     return txt
 
 def reindent(txt):
+    """ reindents according to { and } braces.  strips all whitespace,
+        possibly not smartest thing to do.  oh well.
+    """
     res = ''
     indent = 0
     for l in txt.split("\n"):
@@ -27,10 +57,17 @@ def java2python(txt):
     txt = reindent(txt)
     txt = txt.replace("/*", '"""')
     txt = txt.replace("*/", '"""')
+    txt = txt.replace("<>", '!=')
+    txt = txt.replace("null", 'None')
+    txt = txt.replace("true", 'True')
+    txt = txt.replace("false", 'False')
     txt = txt.replace("//", '#')
     txt = txt.replace("this.", 'self.')
+    txt = txt.replace("else if", 'elif')
+    txt = txt.replace("new ", '')
     l = txt.split("\n")
     l = map(java2pythonlinebyline, l)
+    txt = txt.replace("}", '')
     return '\n'.join(l)
 
 if __name__ == "__main__":
