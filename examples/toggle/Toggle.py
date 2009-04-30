e6c5cd00bdf5535f5f09804ded3a2373bcd81038
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-04-30 10:21:29 +0000

    extend toggle example
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@567 7a2bd370-bda8-463c-979e-2900ccfb811e

--- examples/toggle/Toggle.py
+++ examples/toggle/Toggle.py
@@ -9,7 +9,9 @@ from pyjamas.ui.CustomButton import CustomButtom
 from pyjamas.ui.ToggleButton import ToggleButton
 from pyjamas.ui.PushButton import PushButton
 from pyjamas               import DOM
-
+from pyjamas.ui.Image      import Image
+from pyjamas.ui.VerticalPanel import VerticalPanel
+from pyjamas.ui.HorizontalPanel import HorizontalPanel
 
 class Toggle:
     def onModuleLoad(self):
@@ -17,23 +19,48 @@ class Toggle:
         self.label = Label("Not set yet")
         
         self.button = Button("Probe button", self)
-        self.toggle = ToggleButton("up","down")
-        RootPanel().add(self.label)
-        RootPanel().add(self.button)
-        RootPanel().add(self.toggle)
-        self.i = 0
+        self.image_up = Image("./images/logo.png")
+        self.image_up3 = Image("./images/logo.png")
+        self.image_down = Image("./images/logo.png")
+        self.image_down3 = Image("./images/logo.png")
+        self.toggle = ToggleButton(self.image_up, self.image_down, self)
+        self.toggle2 = ToggleButton("up", "down")
+        self.toggle3 = ToggleButton(self.image_up3, self.image_down3)
+        
+        self.vpanel = VerticalPanel()
+        self.togglePanel = HorizontalPanel()
+        self.togglePanel.setSpacing(10)
+        
+        self.togglePanel.add(self.toggle)
+        self.togglePanel.add(self.toggle2)
+        self.togglePanel.add(self.toggle3)
+        
+        self.vpanel.add(self.label)
+        self.vpanel.add(self.button)
+        self.vpanel.add(self.togglePanel)
+        
+        RootPanel().add(self.vpanel)
+        self.i = False
         
     def onClick(self, sender):
         if sender == self.button:
             if self.i: 
-                self.i = 0
+                self.i = False
+                text = ">>>>UP<<<<"
                 self.toggle.setCurrentFace(self.toggle.getUpFace())
             else:
-                self.i = 1
+                self.i = True
+                text = ">>>DOWN<<<"
                 self.toggle.setCurrentFace(self.toggle.getDownFace())
-            self.label.setText("self.toggle.style_name: "+
-                                self.toggle.style_name+", self.toggle.getStyleName():"+
-                                self.toggle.getStyleName()+" ")
+            #self.label.setText("self.toggle.style_name: "+
+            #                    self.toggle.style_name+", self.toggle.getStyleName():"+
+            #                    self.toggle.getStyleName()+" ")
+            self.label.setText(text)
+        elif sender == self.toggle:
+            text = ">>>DOWN<<<"
+            if self.i: text = ">>>>UP<<<<"
+            self.i = not self.i
+            self.label.setText(text+" isdown: "+self.toggle.isDown())
 
 
 if __name__ == "__main__":
