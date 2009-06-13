7600796bf640bbf4a332ab4b866f5e87b9fbb8ab
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-06-13 09:55:45 +0000

    Added to forgotten files for unittest cirular imports.
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@633 7a2bd370-bda8-463c-979e-2900ccfb811e

---
+++ examples/libtest/imports/circ1.py
@@ -0,0 +1,6 @@
+
+import imports
+
+imports.exec_order.append('circ1-1')
+import imports.circ2
+imports.exec_order.append('circ1-2')
---
+++ examples/libtest/imports/circ2.py
@@ -0,0 +1,6 @@
+
+import imports
+
+imports.exec_order.append('circ2-1')
+import imports.circ1
+imports.exec_order.append('circ2-2')
