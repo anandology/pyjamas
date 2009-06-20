5e886957eedd672d46ea43e17b281159fdb10a25
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-06-20 15:05:04 +0000

    make JSONRPC example useable without apache2; add mod_python starter
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@655 7a2bd370-bda8-463c-979e-2900ccfb811e

--- examples/jsonrpc/public/services/EchoService.py
+++ examples/jsonrpc/public/services/EchoService.py
@@ -14,7 +14,12 @@ class Service:
         return msg.lower()
 
 
-from jsonrpc.cgihandler import handleCGIRequest
-
-handleCGIRequest(Service())
-
+if __name__ == '__main__':
+    # this is if JSONService.py is run as a CGI
+    from jsonrpc.cgihandler import handleCGIRequest
+    handleCGIRequest(Service())
+else:
+    # this is if JSONService.py is run from mod_python:
+    # rename .htaccess.mod_python to .htaccess to activate,
+    # and restart Apache2
+    from jsonrpc.apacheServiceHandler import handler 
