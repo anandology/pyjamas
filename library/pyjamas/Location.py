7934ae1531e6b3d2be9e21ddfd8d8c9169f98c50
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-06-22 18:16:45 +0000

    add pyjd Location.py
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@661 7a2bd370-bda8-463c-979e-2900ccfb811e

--- library/pyjamas/Location.py
+++ library/pyjamas/Location.py
@@ -1,3 +1,5 @@
+import pyjslib
+
 def makeUrlDict(s):
     dict = {}
     pairs = s.split("&")
@@ -23,69 +25,72 @@ class Location:
     """
     def __init__(self, location):
         self.location = location
+        self.searchDict = None
     
     def getHash(self):
-        return self.location.hash
+        return self.location.props.hash
     
     def getHashDict(self):
         if not self.hashDict or self.hashDictHash != self.getHash():
             self.hashDictHash = self.getHash()
-            self.hashDict = makeUrlDict(self.getHash().slice(1))
+            self.hashDict = makeUrlDict(self.getHash()[1:])
         return self.hashDict
     
     def getHost(self):
-        return self.location.host
+        return self.location.props.host
     
     def getHostname(self):
-        return self.location.hostname
+        return self.location.props.hostname
     
     def getHref(self):
-        return self.location.href
+        return self.location.props.href
     
     def getPageHref(self):
         """
         Return href with any search or hash stripped
         """
-        href = self.location.href
+        href = self.location.props.href
         if href.find("?"): href = href.split("?")[0]
         if href.find("#"): href = href.split("#")[0]
         return href
     
     def getPathname(self):
-        return self.location.pathname
+        return self.location.props.pathname
     
     def getPort(self):
-        return self.location.port
+        return self.location.props.port
     
     def getProtocol(self):
-        return self.location.protocol
+        return self.location.props.protocol
         
     def getSearch(self):
-        return ""+self.location.search
+        return ""+self.location.props.search
     
     def getSearchDict(self):
-        if not hasattr(self, 'searchDict'):
-            self.searchDict = makeUrlDict(self.getSearch().slice(1))
+        if pyjslib.isString(self.location):
+            return {}
+        if not self.searchDict:
+            self.searchDict = makeUrlDict(self.getSearch()[1:])
         return self.searchDict
 
     def getSearchVar(self, key):
         searchDict = self.getSearchDict()
-        return searchDict.get(key)
+        return searchDict[key]
     
     def reload(self):
-        self.location.reload()
+        self.location.props.reload()
         
     def setHref(self, href):
-        self.location.href = href
+        self.location.props.href = href
 
     def setSearch(self, search):
-        self.location.search = search
+        self.location.props.search = search
         
     def setSearchDict(self, searchDict):
         self.setSearch(makeUrlStringFromDict(searchDict))
         
     def setHash(self, hash):
-        self.location.hash = hash
+        self.location.props.hash = hash
         
     def setHashDict(self, hashDict):
         self.setHash(makeUrlStringFromDict(hashDict))
