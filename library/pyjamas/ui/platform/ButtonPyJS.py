bd4a7cfd367a5d93b26bb96a04c917980da30bb4
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-07-01 16:36:36 +0000

    remove unneeded ButtonPyJS.py
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@724 7a2bd370-bda8-463c-979e-2900ccfb811e

--- library/pyjamas/ui/platform/ButtonPyJS.py
+++
@@ -1,9 +0,0 @@
-class Button(ButtonBase):
-
-    def adjustType(self, button):
-        JS("""
-        if (button.type == 'submit') {
-            try { button.setAttribute("type", "button"); } catch (e) { }
-        }
-        """)
-
