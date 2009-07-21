b9dbc5632f4787b75261e418fc2f10b5193343ce
bernddorn <bernddorn@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-07-21 14:52:20 +0000

    fix a bug in DOM.toString on safari
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@988 7a2bd370-bda8-463c-979e-2900ccfb811e

--- library/__safari__/pyjamas/DOM.py
+++ library/__safari__/pyjamas/DOM.py
@@ -1,3 +1,17 @@
+
+def toString(elem):
+    # need to override because safari does not like '' as inner
+    # html. just leave it out so far, don't know what this should do
+    # anyways
+    JS("""
+    var temp = elem.cloneNode(true);
+    var tempDiv = $doc.createElement("DIV");
+    tempDiv.appendChild(temp);
+    outer = tempDiv.innerHTML;
+    //temp.innerHTML = " ";
+    return outer;
+    """)
+
 def getAbsoluteLeft(elem):
     JS("""
     var left = 0;
