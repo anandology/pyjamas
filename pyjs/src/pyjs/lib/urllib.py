298d5fde90812c17f62c8dddde9cf26d8362d014
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-07-27 21:45:49 +0000

    Implements parts of urllib (quote, urlencode).
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>
    
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@1016 7a2bd370-bda8-463c-979e-2900ccfb811e

---
+++ pyjs/src/pyjs/lib/urllib.py
@@ -0,0 +1,35 @@
+def quote(s):
+    """
+    This function is equivalent to urllib.quote().
+
+    Example:
+
+    >>> quote("hey")
+    'hey'
+    >>> quote("$%&/?/+ s")
+    '%24%25%26/%3F/%2B%20s'
+
+    """
+    JS("""return encodeURIComponent(s);""")
+
+def urlencode(d):
+    """
+    Equivalent of urllib.urlencode().
+
+    Examples:
+    >>> urlencode({"a": 34, "bbb": "ccc"})
+    'a=34&bbb=ccc'
+    >>> urlencode({"a": 34, "bbb": "$%&/?/+ s"})
+    'a=34&bbb=%24%25%26/%3F/%2B%20s'
+    >>> urlencode({"a": 34})
+    'a=34'
+    >>> urlencode({})
+    ''
+
+    """
+    s = ""
+    for i, v in d.iteritems():
+        s += "%s=%s&" % (i, quote(str(v)))
+    if s != "":
+        s = s[:-1]
+    return s
