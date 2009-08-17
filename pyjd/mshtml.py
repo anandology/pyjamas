002125b5b4e4f77aa04d57731d5eb5bfa03758f2
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-08-17 10:44:38 +0000

    bit of a tidyup of mshtml port
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@1187 7a2bd370-bda8-463c-979e-2900ccfb811e

--- pyjd/mshtml.py
+++ pyjd/mshtml.py
@@ -1,10 +1,8 @@
 #import win32traceutil
 
 import traceback
-import win32con
 import sys
 import os
-from ctypes import *
 import time
 import new
 
@@ -14,138 +12,32 @@ import timer
 import encodings
 import encodings.cp437
 
-import _mshtml
 
-import win32con
-
+from windows import *
 from ctypes import *
 from ctypes.wintypes import *
+
 import comtypes
 from comtypes import IUnknown
 from comtypes.automation import IDispatch, VARIANT
 from comtypes.client import wrap, GetModule
 from comtypes.client.dynamic import Dispatch
 
-#from win32com.client import *
-#cast = gencache.GetModuleForProgID('htmlfile')
+import mshtmlevents 
 
 import comtypes.gen
 
 if not hasattr(sys, 'frozen'):
-    _atl = GetModule('atl.dll')
-    _shdv = GetModule('shdocvw.dll')
-    _msxml2 = GetModule('msxml2.dll')
-    _mshtml = GetModule('mshtml.tlb') 
+    GetModule('atl.dll')
+    GetModule('shdocvw.dll')
+    GetModule('msxml2.dll')
+    GetModule('mshtml.tlb') 
 
 from comtypes.gen import SHDocVw
 from comtypes.gen import MSXML2
 from comtypes.gen import MSHTML
 
-kernel32 = windll.kernel32
-user32 = windll.user32
 atl = windll.atl                  # If this fails, you need atl.dll
-
-import win32con
-from ctypes import *
-from comtypes import IUnknown
-from comtypes.automation import VARIANT
-#from comtypes.client import GetEvents, ShowEvents
-import mshtmlevents 
-
-kernel32 = windll.kernel32
-user32 = windll.user32
-
-WNDPROC = WINFUNCTYPE(c_long, c_int, c_uint, c_int, c_int)
-
-class WNDCLASS(Structure):
-    _fields_ = [('style', c_uint),
-                ('lpfnWndProc', WNDPROC),
-                ('cbClsExtra', c_int),
-                ('cbWndExtra', c_int),
-                ('hInstance', c_int),
-                ('hIcon', c_int),
-                ('hCursor', c_int),
-                ('hbrBackground', c_int),
-                ('lpszMenuName', c_char_p),
-                ('lpszClassName', c_char_p)]
-
-class RECT(Structure):
-    _fields_ = [('left', c_long),
-                ('top', c_long),
-                ('right', c_long),
-                ('bottom', c_long)]
-
-class PAINTSTRUCT(Structure):
-    _fields_ = [('hdc', c_int),
-                ('fErase', c_int),
-                ('rcPaint', RECT),
-                ('fRestore', c_int),
-                ('fIncUpdate', c_int),
-                ('rgbReserved', c_char * 32)]
-
-class POINT(Structure):
-    _fields_ = [('x', c_long),
-                ('y', c_long)]
-    
-class MSG(Structure):
-    _fields_ = [('hwnd', c_int),
-                ('message', c_uint),
-                ('wParam', c_int),
-                ('lParam', c_int),
-                ('time', c_int),
-                ('pt', POINT)]
-
-def ErrorIfZero(handle):
-    if handle == 0:
-        raise WinError
-    else:
-        return handle
-
-def _createDiv(doc):
-    div = doc.createElement('div')
-    print div
-    style = div.style
-    print style
-    #style2 = style.QueryInterface(MSHTML.IHTMLStyle2)
-    #style3 = style.QueryInterface(MSHTML.IHTMLStyle3)
-    style.position = 'absolute'
-    print "pos", style.position
-    style.top = 0
-    print "top", style.top
-    sys.stdout.flush()
-    print "left before", style.left
-    style.left = 0
-    print "left", style.left
-    style.overflow = 'scroll'
-    print "overflow", style.overflow
-    style.overflowX = 'hidden'
-    print "overflowX", style.overflowX
-    style.width = 300
-    print "width", style.width
-    style.height = 100
-    print "height", style.height
-    style.scrollbarBaseColor = '#3366CC'
-    style.borderBottom = '2px solid black'
-    style.scrollbarHighlightColor = '#99CCFF'
-    style.scrollbarArrowColor = 'white'
-    div.innerHTML = 'Hello'
-    return div
-
-#b = Dispatch('InternetExplorer.Application')
-#b.Navigate('about:<h1 id=header>Hello</h1><iframe id=frm src="about:"></iframe>')
-#b.Visible = 1
-#doc1 = cast.IHTMLDocument2(b.Document)
-#header = doc1.all.item('header')
-#frm = doc1.all.item('frm')
-#frm2 = doc1.frames('frm')
-#doc2 = cast.IHTMLDocument2(frm2.document)
-#div = doc2.createElement('div')
-#cast.DispHTMLBody(doc2.body).appendChild(div)
-
-#popup = cast.DispHTMLWindow2(doc1.parentWindow).createPopup()
-#doc3 = cast.IHTMLDocument2(popup.document)
-#body = cast.DispHTMLBody(doc3.body)
-#div = _createDiv()
 
 class EventSink(object):
     # some DWebBrowserEvents
@@ -189,32 +81,9 @@ class EventSink(object):
         v = cast(args[1]._.c_void_p, POINTER(VARIANT))[0]
         v.value = True
 
-def addWindowEventListener(self, event_name, cb):
-    #print self, event_name, cb
-    if cb not in self._callbacks:
-        self.connect("browser-event", cb)
-        self._callbacks.append(cb)
-    return self.addWindowEventListener(event_name, True)
-
-def addXMLHttpRequestEventListener(element, event_name, cb):
-    if not hasattr(element, "_callbacks"):
-        element._callbacks = []
-    if cb not in element._callbacks:
-        element.connect("browser-event", cb)
-        element._callbacks.append(cb)
-    return element.addEventListener(event_name)
-
-def addEventListener(element, event_name, cb):
-    if not hasattr(element, "_callbacks"):
-        element._callbacks = []
-    if cb not in element._callbacks:
-        element.connect("browser-event", cb)
-        element._callbacks.append(cb)
-    return element.addEventListener(event_name, True)
-
 fn_txt = """\
 def event_fn(self, *args):
-    print "event %s", self, args, dir(self)
+    print "event %s", self, args
     print "event callbacks", self._listeners
     callbacks = self._listeners.get('%s', [])
     for fn in callbacks:
@@ -275,28 +144,24 @@ class Browser(EventSink):
         self.window_handler = None
         self.node_handlers = {}
 
-        CreateWindowEx = windll.user32.CreateWindowExA
-        CreateWindowEx.argtypes = [c_int, c_char_p, c_char_p, c_int, c_int, c_int, c_int, c_int, c_int, c_int, c_int, c_int]
-        CreateWindowEx.restype = ErrorIfZero
-
         # Create an instance of IE via AtlAxWin.
         atl.AtlAxWinInit()
-        hInstance = kernel32.GetModuleHandleA(None)
+        hInstance = GetModuleHandle(None)
 
         self.hwnd = CreateWindowEx(0,
                               "AtlAxWin",
                               "about:blank",
-                              win32con.WS_OVERLAPPEDWINDOW |
-                              win32con.WS_VISIBLE | 
-                              win32con.WS_HSCROLL | win32con.WS_VSCROLL,
-                              win32con.CW_USEDEFAULT,
-                              win32con.CW_USEDEFAULT,
-                              win32con.CW_USEDEFAULT,
-                              win32con.CW_USEDEFAULT,
-                              win32con.NULL,
-                              win32con.NULL,
+                              WS_OVERLAPPEDWINDOW |
+                              WS_VISIBLE | 
+                              WS_HSCROLL | WS_VSCROLL,
+                              CW_USEDEFAULT,
+                              CW_USEDEFAULT,
+                              CW_USEDEFAULT,
+                              CW_USEDEFAULT,
+                              NULL,
+                              NULL,
                               hInstance,
-                              win32con.NULL)
+                              NULL)
 
         # Get the IWebBrowser2 interface for the IE control.
         self.pBrowserUnk = POINTER(IUnknown)()
@@ -327,8 +192,8 @@ class Browser(EventSink):
 
         # Show Window
         cw = c_int(self.hwnd)
-        windll.user32.ShowWindow(cw, c_int(win32con.SW_SHOWNORMAL))
-        windll.user32.UpdateWindow(cw)
+        ShowWindow(cw, c_int(SW_SHOWNORMAL))
+        UpdateWindow(cw)
 
     def getDomDocument(self):
         return Dispatch(self.pBrowser.Document)
@@ -429,43 +294,29 @@ class Browser(EventSink):
         sys.path.append(pth)
         
     def on_unload_callback(self, *args):
-        windll.user32.PostQuitMessage(0)
+        PostQuitMessage(0)
 
 def MainWin(one_event):
 
     # Pump Messages
     msg = MSG()
     pMsg = pointer(msg)
-    NULL = c_int(win32con.NULL)
     
     while 1:
-        res = windll.user32.GetMessageA( pMsg, NULL, 0, 0)
+        res = GetMessage( pMsg, NULL, 0, 0)
         if res == -1:
             return 0
         if res == 0:
             break 
 
-        windll.user32.TranslateMessage(pMsg)
-        windll.user32.DispatchMessageA(pMsg)
-
-        #print msg.message, msg.wParam, msg.lParam
-        #if msg.message == 161: # win32con.WM_DESTROY:
-        #    windll.user32.PostQuitMessage(0)
+        TranslateMessage(pMsg)
+        DispatchMessageA(pMsg)
 
         if one_event:
             break
 
     return msg.wParam
     
-class ContentInvoker:
-
-    def __init__(self, node, event_fn):
-        self._node = node
-        self._event_fn = event_fn
-
-    def handleEvent(self, event):
-        self._event_fn(self._node, event, False)
-
 global wv
 wv = None
 
