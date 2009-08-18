b134468b09a079a7617e9c1f6b701fced182ed36
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-08-18 09:38:25 +0000

    whoops, string for focus
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@1212 7a2bd370-bda8-463c-979e-2900ccfb811e

--- library/pyjamas/ui/platform/Focusmshtml.py
+++ library/pyjamas/ui/platform/Focusmshtml.py
@@ -3,7 +3,7 @@ def focus(elem):
     # i know this looks weird, but it's to catch the exceptions
     # that might happen on focus and ignore them (the names of which
     # we don't know) but otherwise to create standard errors.
-    if elem and hasattr(elem, focus):
+    if elem and hasattr(elem, 'focus'):
         try:
             elem.focus()
         except:
