ec3b080b2f1922f4989b853db45475d185e314de
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-08-28 18:27:46 +0000

    add all
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@1465 7a2bd370-bda8-463c-979e-2900ccfb811e

---
+++ examples/gcharttestapp/TestGChart00.py
@@ -0,0 +1,14 @@
+
+import GChartTestAppUtil
+from pyjamas.chart.GChart import GChart
+
+
+"""* Empty chart without anything on it except a title and footnotes """
+class TestGChart00 (GChart):
+    def __init__(self):
+        GChart.__init__(self, 150,150)
+        self.setChartTitle(GChartTestAppUtil.getTitle(self))
+        self.setChartFootnotes("Check: Consistent with a 'no data' chart (and it doesn't crash).")
+    
+
+
