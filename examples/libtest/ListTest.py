6884f6b75b1b0d8414bffdb44056416381637085
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-09-10 18:37:35 +0000

    Revert "Added --operator-funcs to enable non-number operation such as [1]+[2]"
    This reverts commit 8a9f266c82aac5b2315fde83d15d6e4aabc9e310.
    
    Conflicts:
    
    	CHANGELOG
    
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@1775 7a2bd370-bda8-463c-979e-2900ccfb811e

--- examples/libtest/ListTest.py
+++ examples/libtest/ListTest.py
@@ -48,9 +48,6 @@ class ListTest(UnitTest):
 
         self.assertTrue( added == [1,2,3,4], "TODO: #222 - a lot of work implementing __coerce__ etc.")
 
-        l1 += l2
-        self.assertTrue(l1 == [1,2,3,4], "l1 += l2")
-
     def testDelete(self):
         value = [0, 1, 2, 3, 4]
         del value[4]
--- pyjs/src/pyjs/browser.py
+++ pyjs/src/pyjs/browser.py
@@ -424,10 +424,7 @@ def build_script():
         descriptors=options.descriptors,
         source_tracking=options.source_tracking,
         line_tracking=options.line_tracking,
-        store_source=options.store_source,
-        inline_code = options.inline_code,
-        operator_funcs = options.operator_funcs,
-    )
+        store_source=options.store_source)
 
     l = BrowserLinker(args,
                       output=options.output,
