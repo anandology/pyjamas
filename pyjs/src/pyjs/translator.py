fe4bfc6355ba4057c150eeee3e82c8a8fce9541e
keesbos <keesbos@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-09-22 12:23:16 +0000

    Bugfix 'locals()': remove X from locals when 'from __pyjamas__ import X'
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@1876 7a2bd370-bda8-463c-979e-2900ccfb811e

--- pyjs/src/pyjs/translator.py
+++ pyjs/src/pyjs/translator.py
@@ -1888,7 +1888,7 @@ var %s = arguments.length >= %d ? arguments[arguments.length-1] : arguments[argu
                 except TranslationError, e:
                     raise TranslationError(e.msg, v, self.module_name)
             elif v.node.name == 'locals':
-                return """pyjslib.dict({%s})""" % (",".join(["'%s': %s" % (pyname, self.lookup_stack[-1][pyname][2]) for pyname in self.lookup_stack[-1] if self.lookup_stack[-1][pyname][0] not in ['global']]))
+                return """pyjslib.dict({%s})""" % (",".join(["'%s': %s" % (pyname, self.lookup_stack[-1][pyname][2]) for pyname in self.lookup_stack[-1] if self.lookup_stack[-1][pyname][0] not in ['__pyjamas__', 'global']]))
             else:
                 if name_type is None:
                     # What to do with a (yet) unknown name?
