0a0b66e1724458c017316f956bf263f214aca635
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-10-09 11:15:27 +0000

    #299 - add DoubleClickListener
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@1980 7a2bd370-bda8-463c-979e-2900ccfb811e

--- library/pyjamas/ui/ClickListener.py
+++ library/pyjamas/ui/ClickListener.py
@@ -1,22 +1,16 @@
 """ 
     Copyright (C) 2008, 2009 - Luke Kenneth Casson Leighton <lkcl@lkcl.net>
-
+  
 """
 from pyjamas import DOM
 from pyjamas.ui import Event
 
-class ClickHandler:
+class ClickHandler(object):
 
     def __init__(self):
         self._clickListeners = []
         
         self.sinkEvents( Event.ONCLICK )
-
-    def onBrowserEvent(self, event):
-        type = DOM.eventGetType(event)
-
-    def onLostFocus(self, sender):
-        pass
 
     def onClick(self, sender=None):
         pass
@@ -25,6 +19,9 @@ class ClickHandler:
         self._clickListeners.append(listener)
 
     def onBrowserEvent(self, event):
+        """Listen to events raised by the browser and call the appropriate 
+        method of the listener (widget, ..) object. 
+        """
         type = DOM.eventGetType(event)
         if type == "click":
             for listener in self._clickListeners:
@@ -35,4 +32,32 @@ class ClickHandler:
 
     def removeClickListener(self, listener):
         self._clickListeners.remove(listener)
+
+class DoubleClickHandler(object):
+
+    def __init__(self):
+        self._doubleclickListeners = []
+        
+        self.sinkEvents( Event.ONDBLCLICK )
+
+    def onDoubleClick(self, sender=None):
+        pass
+
+    def addDoubleClickListener(self, listener):
+        self._doubleclickListeners.append(listener)
+
+    def onBrowserEvent(self, event):
+        """Listen to events raised by the browser and call the appropriate 
+        method of the listener (widget, ..) object. 
+        """
+        type = DOM.eventGetType(event)
+        if type == "dblclick":
+            for listener in self._doubleclickListeners:
+                if hasattr(listener, "onDblClick"):
+                    listener.onDblClick(self)
+                else:
+                    listener(self)
+
+    def removeDoubleClickListener(self, listener):
+        self._doubleclickListeners.remove(listener)
 
