99f9ebcc8b3cf8d194d416b336f487128a8c1d5c
keesbos <keesbos@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-10-29 10:22:03 +0000

    #312 ListTest oops.
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@2137 7a2bd370-bda8-463c-979e-2900ccfb811e

--- examples/libtest/ListTest.py
+++ examples/libtest/ListTest.py
@@ -385,7 +385,7 @@ class ListTest(UnitTest):
         self.assertTrue(l == [1,2,3])
 
         self.assertTrue([1] > [0,1], "[1] > [0,1] bug #311")
-        self.assertTrue([0,1] > [1], "[0,1] > [1]")
+        self.assertTrue([0,1] < [1], "[0,1] < [1]")
 
     def testListContains(self):
         l = [['monkey'], ['patch'], ['fish'], ['chips']]
