fe67d05a0d8a6536c6fe4f0892696eefd4ebafcb
davidsiroky <davidsiroky@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-11-11 22:55:54 +0000

    - coverage test update
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@2283 7a2bd370-bda8-463c-979e-2900ccfb811e

--- examples/libtest/check_coverage.sh
+++ examples/libtest/check_coverage.sh
@@ -8,4 +8,5 @@ if [ ! -d $OUTPUT ]; then mkdir $OUTPUT; fi
 rm $OUTPUT/*,cover
 python-coverage -x compile_only.py
 python-coverage -a -o /usr -d coverage_report
+python-coverage -r -o /usr -d coverage_report
 
