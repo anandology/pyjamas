30460814c5d2d8dcd452c113e289780cd5b54d42
lkcl <lkcl@7a2bd370-bda8-463c-979e-2900ccfb811e>
2009-11-11 17:56:01 +0000

    simplify FocusPanel, rely on use Applier to call setWidget.
    git-svn-id: https://pyjamas.svn.sourceforge.net/svnroot/pyjamas/trunk@2273 7a2bd370-bda8-463c-979e-2900ccfb811e

--- library/pyjamas/ui/FocusPanel.py
+++ library/pyjamas/ui/FocusPanel.py
@@ -28,17 +28,16 @@ class FocusPanel(SimplePanel, FocusHandler, KeyboardHandler,
                           MouseHandler, ClickHandler,
                           FocusMixin):
 
-    def __init__(self, child=None):
-
-        SimplePanel.__init__(self, Focus.createFocusable())
+    def __init__(self, **kwargs):
+        """ pass in Widget={the widget} so that Applier will call setWidget.  
+        """
 
         FocusHandler.__init__(self)
         KeyboardHandler.__init__(self)
         ClickHandler.__init__(self)
         MouseHandler.__init__(self)
 
-        if child:
-            self.setWidget(child)
+        SimplePanel.__init__(self, Focus.createFocusable(), **kwargs)
 
 Factory.registerClass('pyjamas.ui.FocusPanel', FocusPanel)
 
