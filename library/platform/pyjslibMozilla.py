27bd1cba9b94be1080babfa02a8e4eb3b9a51943
thoka <thomas.kalka@googlemail.com>
2010-09-07 23:14:25 +0200

    check_JS conversion

--- library/platform/pyjslibMozilla.py
+++ library/platform/pyjslibMozilla.py
@@ -1,6 +1,5 @@
 class List:
     def index(self, value, start=0):
         JS("""
-        return this.l.indexOf(value, start);
+        return this.l.indexOf(@{{value}}, @{{start}});
         """)
-
