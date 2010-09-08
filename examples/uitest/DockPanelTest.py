0b4e9be96409875bab6c940a2216245d71b1d786
Luke Leighton <lkcl@lkcl.net>
2010-09-08 14:26:52 +0100

    add DockPanelTest

---
+++ examples/uitest/DockPanelTest.py
@@ -0,0 +1,38 @@
+from pyjamas.ui.RootPanel import RootPanel
+from pyjamas.ui.DockPanel import DockPanel
+from pyjamas.ui.Label import Label
+
+from UnitTest import UnitTest
+
+from __pyjamas__ import doc
+
+class DockPanelTest(UnitTest):
+
+    def testDockAdd(self):
+        self.d = DockPanel()
+        RootPanel('tests').add(self.d)
+        self.write_test_output('adddockpanel')
+
+        if not RootPanel('tests').remove(self.d):
+            self.fail("DockPanel added but apparently not owned by RootPanel()")
+        self.write_test_output('removedockpanel')
+
+    def testDockAddCentre(self):
+        self.d = DockPanel()
+        RootPanel('tests').add(self.d)
+        l = Label("Hello World (label)", StyleName='teststyle')
+        self.d.add(l, DockPanel.CENTER)
+        self.write_test_output('addcentrelabel')
+
+        self.d.remove(l)
+        self.write_test_output('removecentrelabel')
+
+        l2 = Label("Hello World 2 (label)", StyleName='teststyle')
+
+        self.d.add(l2, DockPanel.CENTER)
+        self.write_test_output('addcentrelabel2')
+
+        if not RootPanel('tests').remove(self.d):
+            self.fail("DockPanel added but apparently not owned by RootPanel()")
+        self.write_test_output('removedock')
+
