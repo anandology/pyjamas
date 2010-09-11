anandology/pyjamas

commit 6f3b625872b4618213e63677c3e798ca52a0c129

whitespace cleanup

library/pyjamas/ui/__init__.py
@@ -52,18 +52,20 @@ class Applier(object):
 
     def apply(self, **kwargs):
 
-        if kwargs:
-            k = kwargs.keys()
-            l = len(k)
-            i = -1
-            while i < l-1:
-                i += 1
-                prop = k[i]
-                fn = getattr(self, "set%s" % prop, None)
-                if fn:
-                    args = kwargs[prop]
-                    if isinstance(args, tuple):
-                        fn(*args)
-                    else:
-                        fn(args)
+        if not kwargs:
+            return
+        k = kwargs.keys()
+        l = len(k)
+        i = -1
+        while i < l-1:
+            i += 1
+            prop = k[i]
+            fn = getattr(self, "set%s" % prop, None)
+            if not fn:
+                return
+            args = kwargs[prop]
+            if isinstance(args, tuple):
+                fn(*args)
+            else:
+                fn(args)
 

commit ded8c19ad6a3d97ba053d78a5a2d5282ecbdf70e

"continue", not "return"; fixing bug introduced by Luke in 6f3b625872b4618213e63677c3e798ca52a0c129 "whitespace cleanup".

library/pyjamas/ui/__init__.py
@@ -81,7 +81,7 @@ class Applier(object):
         for (prop, args) in kwargs.items():
             fn = getattr(self, "set%s" % prop, None)
             if not fn:
-                return
+                continue
             args = kwargs[prop]
             if isinstance(args, tuple):
                 fn(*args)