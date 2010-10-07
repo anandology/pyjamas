ff783ac4f873769a6da1e27ca7cfa80bc9c634cf
Luke Leighton <lkcl@lkcl.net>
2010-10-07 01:49:05 +0100

    reinstate botch-job which screwed Slide.py

--- examples/slideshow/Slide.py
+++ examples/slideshow/Slide.py
@@ -1,3 +1,10 @@
+from Sink import Sink, SinkInfo
+from pyjamas.ui.HTML import HTML
+from pyjamas.ui.VerticalPanel import VerticalPanel
+from SlideLoader import SlideLoader
+from pyjamas.HTTPRequest import HTTPRequest
+from pyjamas import Window
+
 def esc(txt):
     return txt
 
@@ -31,70 +38,98 @@ def ts(txt):
         r.append(urlmap(line))
     return '<br />'.join(r)
 
-def markdown(text):
-    result = ''
-    ul_stack1 = 0
-    ul_stack2 = 0
-    doing_code = 0
-    txt = ''
-    text += '\n'
-    for line in text.split("\n"):
-        if doing_code:
-            if line == "}}":
-                doing_code = 0
-                line = "</pre>"
-                txt += line
+class Slide(Sink):
+    def __init__(self):
+
+        Sink.__init__(self)
+
+        text="<div class='infoProse'>This is the Kitchen Sink sample.  "
+
+        self.vp = VerticalPanel()
+        self.initWidget(self.vp)
+        self.loaded = False
+
+    def onShow(self):
+
+        if self.loaded:
+            return 
+
+        name = self.name.replace(" ", "_")
+        name = name.lower()
+        HTTPRequest().asyncGet("%s.txt" % name, SlideLoader(self))
+
+    def setSlide(self, text):
+        self.loaded = True
+        ul_stack1 = 0
+        ul_stack2 = 0
+        doing_code = 0
+        txt = ''
+        text += '\n'
+        for line in text.split("\n"):
+            if doing_code:
+                if line == "}}":
+                    doing_code = 0
+                    line = "</pre>"
+                    txt += line
+                    self.vp.add(HTML(txt))
+                    txt = ''
+                    continue
+                if line:
+                    txt += line
+                txt += "\n"
+                continue
+                
+            line = line.strip()
+            ul_line = False
+            ul_line2 = False
+            add = False
+            if not line:
+                line = "&nbsp;"
+            elif line[:2] == "{{":
+                doing_code = 1
+                if len(line) > 2:
+                    line = "<pre class='slide_code'>%s" % line[2:]
+                else:
+                    line = "<pre class='slide_code'>"
+            elif line[:2] == '= ' and line[-2:] == ' =':
+                line = "<h1 class='slide_heading1'>%s</h1>" % line[2:-2]
+            elif line[:3] == '== ' and line[-3:] == ' ==':
+                line = "<h2 class='slide_heading2>%s</h2>" % line[3:-3]
+            elif line[:2] == '* ':
+                if not ul_stack1:
+                    txt += "<ul class='slide_list1'>\n"
+                line = "<li class='slide_listitem1'/>%s\n" % ts(line[2:])
+                ul_stack1 = True
+                ul_line = True
+            elif line[:3] == '** ':
+                if not ul_stack2:
+                    txt += "<ul class='slide_list2'>\n"
+                line = "<li class='slide_listitem2'/>%s\n" % ts(line[2:])
+                ul_stack2 = True
+                ul_line2 = True
+                ul_line = True
+            else:
+                if not doing_code:
+                    line = "<p class='slide_para'>%s</p>" % line
+            if ul_stack2 and not ul_line2:
+                ul_stack2 = False
+                txt += "</ul>\n"
+            if ul_stack1 and not ul_line:
+                ul_stack1 = False
+                txt += "</ul>\n"
+            if not ul_stack2 and not ul_stack1 and not doing_code:
+                add = True
+            txt += line
+            if add:
                 self.vp.add(HTML(txt))
                 txt = ''
-                continue
-            if line:
-                txt += line
-            txt += "\n"
-            continue
-            
-        line = line.strip()
-        ul_line = False
-        ul_line2 = False
-        add = False
-        if not line:
-            line = "&nbsp;"
-        elif line[:2] == "{{":
-            doing_code = 1
-            if len(line) > 2:
-                line = "<pre class='slide_code'>%s" % line[2:]
-            else:
-                line = "<pre class='slide_code'>"
-        elif line[:2] == '= ' and line[-2:] == ' =':
-            line = "<h1 class='slide_heading1'>%s</h1>" % line[2:-2]
-        elif line[:3] == '== ' and line[-3:] == ' ==':
-            line = "<h2 class='slide_heading2>%s</h2>" % line[3:-3]
-        elif line[:2] == '* ':
-            if not ul_stack1:
-                txt += "<ul class='slide_list1'>\n"
-            line = "<li class='slide_listitem1'/>%s\n" % ts(line[2:])
-            ul_stack1 = True
-            ul_line = True
-        elif line[:3] == '** ':
-            if not ul_stack2:
-                txt += "<ul class='slide_list2'>\n"
-            line = "<li class='slide_listitem2'/>%s\n" % ts(line[2:])
-            ul_stack2 = True
-            ul_line2 = True
-            ul_line = True
-        else:
-            if not doing_code:
-                line = "<p class='slide_para'>%s</p>" % line
-        if ul_stack2 and not ul_line2:
-            ul_stack2 = False
-            txt += "</ul>\n"
-        if ul_stack1 and not ul_line:
-            ul_stack1 = False
-            txt += "</ul>\n"
-        if not ul_stack2 and not ul_stack1 and not doing_code:
-            add = True
-        txt += line
-        if add:
-            result += txt
-            txt = ''
-    return result
+
+    def onError(self, text, code):
+        self.vp.clear()
+        self.vp.add(HTML("TODO: Slide '%s' not loaded" % self.name))
+        self.vp.add(HTML(text))
+        self.vp.add(HTML(code))
+        
+def init(name, desc):
+    return SinkInfo(name, desc, Slide)
 
