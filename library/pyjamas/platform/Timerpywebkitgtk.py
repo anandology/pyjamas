17125507250d8775b250b69e8b157217f72007c1
Luke Leighton <lkcl@lkcl.net>
2010-10-12 23:07:21 +0100

    use new setTimeout, clearTimeout and setInterval functions from pywebkitgtk

--- library/pyjamas/platform/Timerpywebkitgtk.py
+++ library/pyjamas/platform/Timerpywebkitgtk.py
@@ -1,11 +1,18 @@
+
 class Timer:
 
     def __setTimeout(self, delayMillis):
 
-        return pyjd.gobject.timeout_add(delayMillis, self.__fire)
+        mf = get_main_frame()
+        return mf.get_dom_window().setTimeout(self.__fire, delayMillis)
 
     def __clearTimeout(self,timer):
-        timer.cancel()
+        mf = get_main_frame()
+        return mf.get_dom_window().clearTimeout(timer)
+
+    def __setInterval(self, periodMillis):
+        mf = get_main_frame()
+        return mf.get_dom_window().setInterval(self.__fire, periodMillis)
 
     __clearInterval = __clearTimeout
 
