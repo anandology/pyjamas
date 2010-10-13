9f8b6e5152cc3b9171c272738d4315368290e677
Luke Leighton <lkcl@lkcl.net>
2010-10-13 14:40:52 +0100

    add sort buttons, disclosure panel

--- examples/funnysortedgridthing/SortedGridThing.py
+++ examples/funnysortedgridthing/SortedGridThing.py
@@ -21,15 +21,47 @@ class OddGridWidget(DockPanel):
         cf = self.setCellHeight(self.header, "50px")
         cf = self.setCellHeight(self.sp, "100%")
 
+        self.sortcol = 0
+
     def setData(self, data):
-        rows = len(data)
+        self.data = data
+        self.redraw()
+
+    def sortfn(self, row1, row2):
+        return cmp(row1[self.sortcol], row2[self.sortcol])
+
+    def redraw(self):
+        self.data.sort(self.sortfn)
+
+        rows = len(self.data)
         cols = 0
         if rows > 0:
-            cols = len(data[0])
+            cols = len(self.data[0])
+
         self.grid.resize(rows, cols)
-        for (nrow, row) in enumerate(data):
+        self.header.resize(1, cols)
+
+        cf = self.grid.getCellFormatter()
+
+        for (nrow, row) in enumerate(self.data):
             for (ncol, item) in enumerate(row):
                 self.grid.setHTML(nrow, ncol, str(item))
+                cf.setWidth(nrow, ncol, "200px")
+
+        cf = self.header.getCellFormatter()
+        self.sortbuttons = []
+        for ncol in range(cols):
+            sb = Button("sort col %d" % ncol)
+            sb.addClickListener(self)
+            self.header.setWidget(0, ncol, sb)
+            cf.setWidth(0, ncol, "200px")
+            self.sortbuttons.append(sb)
+
+    def onClick(self, sender):
+        for (ncol, b) in enumerate(self.sortbuttons):
+            if sender == b:
+                self.sortcol = ncol
+                self.redraw()
 
 data = [["hello", "fred", 52],
         ["bye", "joe", 98],
@@ -45,6 +77,8 @@ if __name__ == '__main__':
     pyjd.setup("public/SortedGridThing.html")
     ogw = OddGridWidget(Width="600px", Height="200px", StyleName="ogw")
     ogw.setData(data)
-    RootPanel().add(ogw)
+    dp = DisclosurePanel("Click to disclose / hide", True, Width="602px")
+    dp.add(ogw)
+    RootPanel().add(dp)
     pyjd.run()
 
