0ae6d2814d4113d56a5c367ac241c8252a712d6e
jwashin <jwashin@zif.hillstreet.home>
2010-11-04 07:17:51 -0400

    IE-specific dnd stuff goes into its proper location.

---
+++ library/__ie6__/pyjamas/dnd/DNDHelper.py
@@ -0,0 +1,50 @@
+
+def getComputedStyle(element, style=None):
+    """
+    Get computed style of an element, like in
+    http://efreedom.com/Question/1-1848445/Duplicating-Element-Style-JavaScript
+    """
+    element_style = element.currentStyle
+    if style:
+        return element_style[style]
+    return element_style
+
+def getScrollOffsets():
+    """
+    Get the window scroll offset values.
+    We only need these if pageX, pageY are not provided.
+
+    The uncommented formulation seems to work.
+    """
+
+#    st = Window.getScrollTop()
+#    sl = Window.getScrollLeft()
+#    return sl, st
+
+# http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
+    try:
+        scrollOffsetY = doc().body.scrollTop
+        scrollOffsetX = doc().body.scrollLeft
+        return scrollOffsetX, scrollOffsetY
+    except:
+        scrollOffsetY = doc().documentElement.scrollTop
+        scrollOffsetX = doc().documentElement.scrollLeft
+        return scrollOffsetX, scrollOffsetY
+
+def eventCoordinates(event):
+    """ Get the absolute coordinates of a mouse event.
+    http://www.quirksmode.org/js/events_properties.html#position
+    """
+    offsetX, offsetY = getScrollOffsets()
+    pageX = event.clientX + offsetX
+    pageY = event.clientY + offsetY
+    return pageX, pageY
+
+
+class DragEvent:
+
+    def setTarget(self, target=None):
+        if target is not None:
+            self.srcElement = target
+        else:
+            self.srcElement = DOM.eventGetTarget(self.evt)
