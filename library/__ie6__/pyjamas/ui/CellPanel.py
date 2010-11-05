4c555d01a2766d2206b5fbc3e384015e9fcfe768
Kees Bos <cornelis.bos@gmail.com>
2010-11-05 19:23:03 +0100

    IE cannot remove border attribute

---
+++ library/__ie6__/pyjamas/ui/CellPanel.py
@@ -0,0 +1,7 @@
+
+class CellPanel(ComplexPanel):
+    def setBorderWidth(self, width):
+        if width is None:
+            DOM.setAttribute(self.table, "border", None)
+        else:
+            DOM.setAttribute(self.table, "border", str(width))
