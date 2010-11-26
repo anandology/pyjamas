f1a0f70fb267cff956ef0c230d789cafd556e6bc
Waldemar Kornewald <wkornewald>
2010-11-26 18:08:16 +0100

    removed unnecessary indent (which never got detented, btw)

--- pyjs/src/pyjs/translator.py
+++ pyjs/src/pyjs/translator.py
@@ -2734,7 +2734,7 @@ var %(e)s_name = (typeof %(e)s.__name__ == 'undefined' ? %(e)s.name : %(e)s.__na
 %(s)s%(p)s.__module__ = '%(module)s';""" % {'s': self.spacing(), 'p': local_prefix, 'module': self.module_name})
 
         if self.function_argument_checking or self.module_name == 'pyjslib':
-            self.w( self.indent() + "%(p)s.__md5__ = '%(m)s';" % {'p': local_prefix, 'm': current_klass.__md5__})
+            self.w( self.spacing() + "%(p)s.__md5__ = '%(m)s';" % {'p': local_prefix, 'm': current_klass.__md5__})
 
         self.push_lookup(name_scope)
         for child in node.code:
