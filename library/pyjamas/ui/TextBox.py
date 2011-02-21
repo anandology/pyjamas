edc59fa75ee80445f09dd1a88cae34cfd553dc85
Kees Bos <cornelis.bos@gmail.com>
2011-02-21 09:37:33 +0100

    HTML5 extension to TextBox (issues #530 and #549)

--- library/pyjamas/ui/TextBox.py
+++ library/pyjamas/ui/TextBox.py
@@ -18,9 +18,27 @@ from pyjamas import Factory
 from TextBoxBase import TextBoxBase
 
 class TextBox(TextBoxBase):
+    '''
+    Use Kind to set a HTML5 type
 
-    _props = [("maxLength", "Max Length", "MaxLength", None),
-             ("visibleLength", "Visible Length", "VisibleLength", None),
+    Attributes supported:
+        Kind
+        MaxLength
+        Min
+        Max
+        Placeholder
+        Required
+        Step
+        VisibleLength
+    '''
+
+    _props = [("kind", "Kind", "Kind", None),
+              ("maxLength", "Max Length", "MaxLength", None),
+              ("min", "Min", "Min", None),
+              ("max", "Max", "Max", None),
+              ("placeholder", "Place Holder", "PlaceHolder", None),
+              ("step", "Step", "Step", None),
+              ("visibleLength", "Visible Length", "VisibleLength", None),
             ]
 
     def __init__(self, **ka):
@@ -35,11 +53,41 @@ class TextBox(TextBoxBase):
     def getMaxLength(self):
         return DOM.getIntAttribute(self.getElement(), "maxLength")
 
+    def getKind(self):
+        return DOM.getAttribute(self.getElement(), "type")
+
+    def getMin(self):
+        return DOM.getAttribute(self.getElement(), "min")
+
+    def getMax(self):
+        return DOM.getAttribute(self.getElement(), "max")
+
+    def getPlaceholder(self):
+        return DOM.getAttribute(self.getElement(), "placeholder")
+
+    def getStep(self):
+        return DOM.getAttribute(self.getElement(), "step")
+
     def getVisibleLength(self):
         return DOM.getIntAttribute(self.getElement(), "size")
 
     def setMaxLength(self, length):
         DOM.setIntAttribute(self.getElement(), "maxLength", length)
+
+    def setKind(self, kind):
+        DOM.setAttribute(self.getElement(), "type", kind)
+
+    def setMin(self, min):
+        DOM.setAttribute(self.getElement(), "min", min)
+
+    def setMax(self, max):
+        DOM.setAttribute(self.getElement(), "max", max)
+
+    def setPlaceholder(self, placeholder):
+        DOM.setIntAttribute(self.getElement(), "placeholder", placeholder)
+
+    def setStep(self, step):
+        DOM.setAttribute(self.getElement(), "step", step)
 
     def setVisibleLength(self, length):
         DOM.setIntAttribute(self.getElement(), "size", length)
