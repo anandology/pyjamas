0710f4ca4028c8ee0579608cb5cc9dabfc82e66d
Kees Bos <cornelis.bos@gmail.com>
2011-04-18 14:44:30 +0200

    Fixes for scoping example

--- examples/scoping/Foo.py
+++ examples/scoping/Foo.py
@@ -7,19 +7,31 @@ def b():
 
 class Foo:
     def __init__(self):
+        Window.alert("Next you should see 'in a', 'in b'")
         x = [a, b]       
         for f in x:
             f() 
 
+
 class Bar:
-    def a(self):
-        Window.alert( "in bar a" )
-    def b(self):
-        Window.alert( "in bar b" )
+    def a():
+        Window.alert( "in Bar a" )
+    def b():
+        Window.alert( "in Bar b" )
+
+    Window.alert("Next you should see 'in Bar a', 'in Bar b'")
+    x = [a, b]       
+    for f in x:
+        f() 
 
     def __init__(self):
+
+        def a():
+            Window.alert( "in bar a" )
+        def b():
+            Window.alert( "in bar b" )
+
         Window.alert("you should now see 'in bar a', 'in bar b'")
         x = [a, b]       
         for f in x:
             f() 
-
--- examples/scoping/Scope.py
+++ examples/scoping/Scope.py
@@ -1,8 +1,16 @@
-from Foo import Foo, Bar
+import pyjd # this is dummy in pyjs.
+
+# This is more for a unittest than an example...
 
 class Scope:
 
     def onModuleLoad(self):
+        from Foo import Foo, Bar
         f = Foo()
         b = Bar()
 
+if __name__ == '__main__':
+    pyjd.setup("Scope.html")
+    app = Scope()
+    app.onModuleLoad()
+    pyjd.run()
