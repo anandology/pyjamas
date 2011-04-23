84abd4f0476147c10d8f9a9c679632342d260994
Kees Bos <cornelis.bos@gmail.com>
2011-04-23 06:43:45 +0200

    CanvasProcessing example now uses GWTCanvas

--- examples/canvasprocessing/CanvasProcessing.py
+++ examples/canvasprocessing/CanvasProcessing.py
@@ -1,13 +1,15 @@
-from pyjamas.Canvas2D import Canvas
+from pyjamas.Canvas.GWTCanvas import GWTCanvas
 from pyjamas.ui.RootPanel import RootPanel
 from pyjamas.DOM import getFirstChild
 from pyjamas import Window
+from pyjamas.ui.HTML import HTML
 import math
 from __pyjamas__ import jsinclude
 
 # Include the processing.js in the module scope
 jsinclude("processing.js")
 from __javascript__ import Processing # defined by processing.js
+
 
 p = None
 radius = 50.0
@@ -38,19 +40,26 @@ def mouseMoved():
     nX = p.mouseX
     nY = p.mouseY
 
-class ProcessingCanvas(Canvas):
+class ProcessingCanvas(GWTCanvas):
     def __init__(self):
-        Canvas.__init__(self,0,0)
+        GWTCanvas.__init__(self, 150, 150, 150, 150)
         self.c = getFirstChild(self.getElement())
         self.p = Processing (self.c)
         global p
         p = self.p
     
 if __name__ == '__main__':
+    note = HTML("""
+Note that this is an example of using processing.js.<br>
+See <a href=http://ejohn.org/blog/processingjs/>http://ejohn.org/blog/processingjs/</a> for more info.<br>
+Since processing.js is built for state-of-the-art browser<br>
+that properly implement canvas, IE is not supported.<br>
+<br>
+""")
+    RootPanel().add(note)
     PC = ProcessingCanvas()
     PC.p.setup = setup
     PC.p.draw = draw
     PC.p.mouseMoved = mouseMoved
     PC.p.init()
     RootPanel().add(PC)
-
