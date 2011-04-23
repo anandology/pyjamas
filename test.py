60601f3cbeeed489fca915ea43ca52085acbfecf
Daniel Kluev <dan.kluev@gmail.com>
2011-04-24 04:58:57 +1100

    Improvements for test.py, including some issue tracker integration

--- test.py
+++ test.py
@@ -26,11 +26,30 @@ from optparse import OptionParser
 from os import path
 import subprocess
 import os
+import re
+import urllib2
+import csv
+import collections
 
+test_msg_re = re.compile("""\s*([\w]+) (Known issue|Test failed) \(([\w\/]+)\) \: (.+)$""")
+issue_no_re = re.compile("""\#(\d+)""")
+test_passed_re = re.compile("""\s*(\w+)\: Passed (\d+) \/ (\d+) tests( \((\d+) failed\))?""")
 currentdir = path.abspath(path.dirname(__file__))
 
 class PyjamasTester(object):
     parser = OptionParser()
+    parser.add_option(
+        "--verbose", "-v",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="Show detailed information")
+    parser.add_option(
+        "--notracker",
+        dest="tracker",
+        action="store_false",
+        default=True,
+        help="Do not load data from issue tracker")
     parser.add_option(
         "--cpython",
         dest="cpython",
@@ -94,25 +113,44 @@ class PyjamasTester(object):
         default="examples/",
         help="Path to examples dir"
         )
+    parser.add_option(
+        "--trackerreport",
+        dest="tracker_report",
+        action="store_true",
+        default=False,
+        help="Provide report on tracker issues lacking failing tests"
+        )
         
     def __init__(self):
         self.options, args = self.parser.parse_args()
         
+        self.tracker_url = "http://code.google.com/p/pyjamas/issues/csv"
         if not path.isabs(self.options.pyv8):
             self.options.pyv8 = path.join(currentdir, self.options.pyv8)
         
         if not path.isabs(self.options.examples_path):
             self.options.examples_path = path.join(currentdir, self.options.examples_path)
+            
+        self.testsresults = []
+        self.testsknown = []
         
         if self.options.libtest_run:
-            self.test_libtest_cpython()
+            self._test(self.test_libtest_cpython)
         
         if self.options.utils_run:
-            self.test_utilities()
+            self._test(self.test_utilities)
         
         if self.options.libtest_run:
             if self.options.pyv8_run:
-                self.test_libtest_pyv8()
+                self._test(self.test_libtest_pyv8)
+        
+        self.issues = {}
+        if self.options.tracker:
+            self.get_tracker_issues()
+        
+        self.print_results()
+        
+
 
     def run_cmd(self, cmd=None, opts=None, cwd=None):
         if not cmd:
@@ -135,19 +173,23 @@ class PyjamasTester(object):
                                 env=os.environ
                                 )
         stdout_value, stderr_value = proc.communicate('')
-        print stdout_value
-        print stderr_value
+        return stdout_value, stderr_value
 
     def test_libtest_cpython(self):
-        self.run_cmd(opts="LibTest.py", cwd=path.join(self.options.examples_path, 'libtest'))
+        return self.parse_cmd_output(
+            *self.run_cmd(opts="LibTest.py", cwd=path.join(self.options.examples_path, 'libtest'))
+            )
 
     def test_utilities(self):
-        pass
+        return []
 
     def test_libtest_pyv8(self):
-        self.run_cmd(cmd=self.options.pyv8,
-                     opts=["--strict", "--dynamic '^I18N[.].*.._..'", "LibTest", "`find I18N -name ??_??.py`"],
-                     cwd=path.join(self.options.examples_path, 'libtest'))
+        return self.parse_cmd_output(
+            *self.run_cmd(cmd=self.options.pyv8,
+                          opts=["--strict", "--dynamic '^I18N[.].*.._..'", "LibTest", 
+                                "`find I18N -name ??_??.py`"],
+                          cwd=path.join(self.options.examples_path, 'libtest'))
+            )
 
     def test_libtest_browsers(self):
         pass
@@ -160,7 +202,143 @@ class PyjamasTester(object):
 
     def test_examples_pyjd(self):
         pass
+    
+    def parse_cmd_output(self, stdout_value, stderr_value=None):
+        """
+        Parse stdout/stderr into list of dicts
+        
+        .. TODO:: handle stderr (exceptions)
+        """
+        res = []
+        for line in stdout_value.split('\n'):
+            m = test_msg_re.match(line)
+            if m:
+                groups = m.groups()
+                test = dict(
+                    cls = groups[0],
+                    status = (groups[1] == 'Known issue') and 'known' or 'failed',
+                    name = groups[2],
+                    message = groups[3]
+                    )
+                if test['status'] == 'known':
+                    test['issues'] = issue_no_re.findall(test['message'])
+                res.append(test)
+            else:
+                m = test_passed_re.match(line)
+                if m:
+                    groups = m.groups()
+                    test = dict(
+                        cls = groups[0],
+                        status = "passed",
+                        name = groups[0],
+                        count = int(groups[1])
+                        )
+                    res.append(test)
+        return res
+                    
+        
+        
+    def _test(self, method):
+        name = method.im_func.func_name
+        d = dict(name=name, 
+                 tests=[], failed_list=[], known_list=[],
+                 total=0, passed=0,
+                 failed=0, known=0, err=None)
+        self.testsresults.append(d)
+        try:
+            d['tests'] = method()
+        except Exception, e:
+            print e
+            d['err'] = e
+            return False
+        
+        for test in d['tests']:
+            if test['status'] == 'passed':
+                d['passed'] += test['count']
+            elif test['status'] == 'failed':
+                d['failed'] += 1
+                d['failed_list'].append(test)
+            elif test['status'] == 'known':
+                d['known'] += 1
+                d['known_list'].append(test)
+                self.testsknown.append((d, test))
+        d['total'] = d['passed'] + d['failed'] + d['known']
+        
+    def get_tracker_issues(self):
+        print "Fetching issues csv from %s" % self.tracker_url
+        csv_data = urllib2.urlopen(self.tracker_url)
+        reader = csv.reader(csv_data)
+        issue_cls = None
+        for row in reader:
+            if not issue_cls:
+                issue_cls = collections.namedtuple("Issue", " ".join(row))
+            elif row:
+                issue = issue_cls(*row)
+                self.issues[issue.ID] = issue
+        print "    received %s issues from tracker" % len(self.issues)
+        
+    def print_results(self):
+        """
+        .. TODO:: handle test_pack['err']
+        """
+        print "="*30
+        print " Test results "
+        print "="*30
+        is_failed = False
+        is_bad_issue = False
+        if self.options.verbose:
+            for test_pack in self.testsresults:
+                print "-"*30
+                print ("%(name)s: total %(total)s, passed %(passed)s,"
+                " known %(known)s, failed %(failed)s" % test_pack)                
+                print "-"*30
+                if test_pack['failed'] > 0:
+                    is_failed = True
+                for test in test_pack['known_list']:
+                    print "    Known issue: %(cls).%(name)s: %(message)s" % test
+                for test in test_pack['failed_list']:
+                    print "[!] Failed test: %(cls).%(name)s: %(message)s" % test
+        else:
+            for test_pack in self.testsresults:
+                print ("%(name)s: total %(total)s, passed %(passed)s,"
+                " known %(known)s, failed %(failed)s" % test_pack)
+                if test_pack['failed'] > 0:
+                    is_failed = True
+            for test_pack in self.testsresults:
+                if test_pack['failed_list']:
+                    print "-"*30
+                    print test_pack['name'], "failed tests:"
+                    for test in test_pack['failed_list']:
+                        print "[!] %(cls)s.%(name)s: %(message)s" % test
+        
+        if self.issues:
+            referenced = []
+            for test_pack, test in self.testsknown:
+                for issue_id in test['issues']:
+                    referenced.append(issue_id)
+                    if not issue_id in self.issues:
+                        if not is_bad_issue:
+                            print "-"*30
+                            print "Some issues referenced as known are not open on tracker:"
+                            is_bad_issue = True
+                        print "[!] Issue #%s is not open, referenced by %s:%s.%s: %s" % (
+                            issue_id,
+                            test_pack['name'],
+                            test['cls'], test['name'], test['message'])
 
+            
+            if self.options.tracker_report:
+                print "="*30
+                print " Issues test coverage report"
+                print "="*30
+                for issue_id, issue in self.issues.iteritems():
+                    if not issue_id in referenced:
+                        print "    No test for issue #%(ID)s '%(Summary)s'" % issue._asdict()
+            
+                        
+
+
+        
 
 if __name__ == '__main__':
     PyjamasTester()
