7e0e96c7ad508512a4485eac0b6b676ff9d03cfb
Kees Bos <cornelis.bos@gmail.com>
2011-07-11 10:36:51 +0200

    Removed unused 'from __pyjamas__ import console'

--- library/gwt/ui/Panel.py
+++ library/gwt/ui/Panel.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __pyjamas__ import console
 from pyjamas import Factory
 from pyjamas import DOM
 
